72b57d34ed985a4276d99912fa479c1da3398c31
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-21 10:43:11 +0900

    초마다 반복해서 출력

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
+/*
+특정 시간을 주기로 코드를 반복할때
+setInterval(반복실행할 함수, 반복간격시간(ms단위))
+*/
+
 const [hr, m, s] = document.querySelectorAll('h1 span');
 
-const now = new Date();
-const hours = now.getHours();
-const min = now.getMinutes();
-const sec = now.getSeconds();
+setInterval(timer, 1000);
 
-//Hours값을 setHours가 조건에 12를 뺸값으로 반환
-//반환된 결과값을 다시 바로 setNumbers에 넣어서 0을 붙히도록 처리
+function timer() {
+	const now = new Date();
+	const hours = now.getHours();
+	const min = now.getMinutes();
+	const sec = now.getSeconds();
 
-hr.innerText = setNumbers(setHours(hours));
-m.innerText = setNumbers(min);
-s.innerText = setNumbers(sec);
+	hr.innerText = setNumbers(setHours(hours));
+	m.innerText = setNumbers(min);
+	s.innerText = setNumbers(sec);
 
-//다음과 같이 코드를 줄일 수 있음
-const values = [setHours(hours), min, sec];
-spans.forEach((el, idx) => {
-	el.innerText = setNumbers(values[idx]);
-});
+	return;
+}
 
 function setNumbers(num) {
 	num < 10 && (num = '0' + num);
 	return num;
 }
 
-//현재 js라서 실행 안됨
-//const setNumbers = (num) => num < 10 && (num = '0' + num);
-console.log(setNumbers(hours));
-
 function setHours(num) {
 	num > 12 && (num = num - 12);
 	return num;
 }
-
-//const setNumbers3 = (num) => num > 12 && (num = num - 12);
