f36a136dc0cccf5940b1f331090cd1013b6d59d1
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-21 10:25:39 +0900

    시간값 출력

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
+const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-console.log(setNumbers(hours));
+//Hours값을 setHours가 조건에 12를 뺸값으로 반환
+//반환된 결과값을 다시 바로 setNumbers에 넣어서 0을 붙히도록 처리
+
+hr.innerText = setNumbers(setHours(hours));
+m.innerText = setNumbers(min);
+s.innerText = setNumbers(sec);
+
+//다음과 같이 코드를 줄일 수 있음
+const values = [setHours(hours), min, sec];
+spans.forEach((el, idx) => {
+	el.innerText = setNumbers(values[idx]);
+});
 
 function setNumbers(num) {
-	/*
-	if (num < 10) num = '0' + num;
-	*/
-
-	//조건식 ? (true일때 실행할 구문) : (false일때 실행할 구문)
-	//주의점 : 삼항연산자 구문안에는 대입연산자가 들어올수없기 때문에 괄호로 묶어서 표현식으로 변경
-	//특정 값이 else일때 굳이 실핼하지 않아도 된다면 &&연산자 활용
-	//num < 10 ? (result = '0' + num) : (result = num);
-
-	return num < 10 && (num = '0' + num);
+	num < 10 && (num = '0' + num);
+	return num;
 }
 
-//const setNumbers = num => num < 10 && (num = '0' + num);
+//현재 js라서 실행 안됨
+//const setNumbers = (num) => num < 10 && (num = '0' + num);
+console.log(setNumbers(hours));
+
+function setHours(num) {
+	num > 12 && (num = num - 12);
+	return num;
+}
+
+//const setNumbers3 = (num) => num > 12 && (num = num - 12);
