1322005219551f3797690fab8db81fcedeb43b0c
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-21 11:18:26 +0900

    시간에 따라 클래스 적용

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ const [hr, m, s] = document.querySelectorAll('h1 span');
 
 setInterval(timer, 1000);
 
+//함수 추가
+//원하는 시간마다 바디에 morning, afternoon, evening, night
+//주의점 (밤)특정이상 혹은 일정시간 이하는 &&가 아닌 ||로 표현 가능
+
+function changeClass(hr) {
+	if (hr >= 6 && hr < 12) document.body.classList.add('morning');
+	if (hr >= 12 && hr < 16) document.body.classList.add('afternoon');
+	if (hr >= 16 && hr < 20) document.body.classList.add('evening');
+	if (hr >= 20 || hr < 6) document.body.classList.add('night');
+}
+
 function timer() {
 	const now = new Date();
 	const hours = now.getHours();
@@ -17,7 +28,7 @@ function timer() {
 	m.innerText = setNumbers(min);
 	s.innerText = setNumbers(sec);
 
-	return;
+	changeClass(hours);
 }
 
 function setNumbers(num) {
